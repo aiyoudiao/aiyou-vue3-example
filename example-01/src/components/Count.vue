d0c09abcc41471a6fe5c4306305f32afc969adc4
aiyoudiao <newdiao@163.com>
2021-11-11 10:59:21 +0800

    refactor: 重构"

--- example-01/src/components/Count.vue
+++ example-01/src/components/Count.vue
@@ -1,22 +1,28 @@
 <template>
     <div @click="add">
         <p>-----------点击里面的数字-------------</p>
-        {{count}}
+        {{count}} --两倍--> {{double}}
         <p>-----------点击里面的数字-------------</p>
     </div>
+    <button @click="asyncAdd">async add</button>
 </template>
 
 <script setup>
 import {computed} from 'vue'
-import { useStore } from '../store/gvuex.js';
+// import { useStore } from '../store/gvuex.js';
+import { useStore } from 'vuex';
 let store = useStore()
 console.log('store', store)
 
 let count = computed(()=>store.state.count)
-// let double = computed(()=>store.getters.double)
+let double = computed(()=>store.getters.double)
 
 function add(){
     store.commit('add')
+}
+
+function asyncAdd(){
+    store.dispatch('asyncAdd')
 }
 
 </script>
--- example-01/src/store/index.js
+++ example-01/src/store/index.js
@@ -1,5 +1,5 @@
-// import { createStore } from 'vuex'
-import { createStore } from './gvuex.js'
+import { createStore } from 'vuex'
+// import { createStore } from './gvuex.js'
 
 export const store = createStore({
     state(){
@@ -12,6 +12,15 @@ export const store = createStore({
             return state.count * 2
         }
     },
+    // 异步的数据变化
+    actions: {
+        asyncAdd({commit}) {
+            setTimeout(() => {
+                commit('add')
+            },1000)
+        }
+    },
+    // 同步的数据变化
     mutations: {
         add(state) {
             state.count++
